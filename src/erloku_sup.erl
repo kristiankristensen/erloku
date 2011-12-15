7ec46e18b25836475827c20d1249a6163bdcc811
Kristian Kristensen <kristian@whzit.dk>
2011-12-15 16:04:05 -0500

    Fixed stupid commenting bug

--- src/erloku_sup.erl
+++ src/erloku_sup.erl
@@ -41,7 +41,7 @@ upgrade() ->
 %% @spec init([]) -> SupervisorTree
 %% @doc supervisor callback.
 init([]) ->
-#    Ip = case os:getenv("WEBMACHINE_IP") of false -> "0.0.0.0"; Any -> Any end,
+%    Ip = case os:getenv("WEBMACHINE_IP") of false -> "0.0.0.0"; Any -> Any end,
     {ok, Dispatch} = file:consult(filename:join(
                          [filename:dirname(code:which(?MODULE)),
                           "..", "priv", "dispatch.conf"])),
@@ -50,7 +50,7 @@ init([]) ->
     WebConfig = [
                  {ip, "0.0.0.0"},
                  {port, Port},
-#                 {log_dir, "priv/log"},
+%                 {log_dir, "priv/log"},
                  {dispatch, Dispatch}],
     Web = {webmachine_mochiweb,
            {webmachine_mochiweb, start, [WebConfig]},
